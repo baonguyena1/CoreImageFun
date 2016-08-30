86d37576f00beca91ff1bdd5f3bbd494071dba56
Bao Nguyen <baonguyena1@gmail.com>
2016-08-30 21:53:54 +0700

    first commit

---
+++ CoreImageFun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreImageFun
+//
+//  Created by Bao Nguyen on 8/30/16.
+//  Copyright © 2016 Bao Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
